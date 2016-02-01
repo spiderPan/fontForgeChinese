26363b5feba28fb799db34b5fe788dca44d63e3c
pan <panbanglanfeng@gmail.com>
2016-01-31 21:14:18 -0500

    had python file done

--- transfer.py
+++ transfer.py
@@ -1,57 +1,37 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python
+import fontforge
+import argparse
 import codecs
-import urllib
-import urllib2
-import os.path
 
-filePath = 'fan.txt'
-REQUEST_HEADERS = {
-"Accept-Language": "en-US,en;q=0.5",
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-"Referer": "http://thewebsite.com",
-"Connection": "keep-alive" 
-}
+# return the character set from the given file
+# file should be utf-8 encoded
+def get_character_set(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
 
-def download_file(url, folder='image'):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+def build_ttf(source_ttf_fname, dest_ttf_fname, char_set_fname):
+    char_set = get_character_set(char_set_fname)
+    source_font = fontforge.open(source_ttf_fname)
+    # select characters
+    for c in char_set:
+        source_font.selection.select(("more", None), ord(c))
+    source_font.selection.invert()
+    # remove all characters we're not interested in
+    for glyph in source_font.selection:
+        try:
+            source_font.removeGlyph(glyph)
+        except ValueError:
+            pass
+    source_font.generate(dest_ttf_fname)
+    source_font.close()
 
-    filename = url.split('/')[-1]
-    complete_filename = os.path.join(folder, filename)
-    try:
-        u = urllib2.urlopen(url)
-        req = urllib2.Request(url, headers=REQUEST_HEADERS)
-        handle = urllib2.urlopen(req)
-    except urllib2.HTTPError, e:
-        print 'We failed with error code - %s.' % e.code
-        return False
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_ttf', help='source ttf')
+    parser.add_argument('dest_ttf', help='lite ttf')
+    parser.add_argument('char_set', help='lite character set')
+    return parser.parse_args()
 
-    data = handle.read()
-
-    with open(complete_filename, 'wb') as f:
-        f.write(data)
-        print 'success'
-
-    return True
-	
-with codecs.open(filePath, 'r', 'utf-8') as f:
-    while True:
-        c = f.read(1)
-        if not c:
-            print 'End of file'
-            break
-        url = 'http://sampler.linotype.com/sam/sam?ID=1390633^&text=' \
-            + c.encode('utf-8') + '^&sizex=4000^&sizey=4000^&fontsize=3000'
-        url = urllib.quote(url.encode('utf-8'))
-        download_file(url)
-        print 'Read a character:', c
-        print 'URL:', url
-
-
-
-
-
-
-			
+if __name__ == '__main__':
+    namespace = parse_args()
+    build_ttf(namespace.source_ttf, namespace.dest_ttf, namespace.char_set)
